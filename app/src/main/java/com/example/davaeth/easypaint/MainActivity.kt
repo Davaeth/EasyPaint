1562b55e40f1d94afa30f7f6262c9262430df398
Davaeth <masterbelki@gmail.com>
2019-06-01 13:39:08 +0200

    Some code cleand up. Made particular path with it's own paint set.

--- app/src/main/java/com/example/davaeth/easypaint/MainActivity.kt
+++ app/src/main/java/com/example/davaeth/easypaint/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.davaeth.easypaint
 
 import android.content.Context
 import android.graphics.*
-import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Attributes
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,9 +21,15 @@ class MainActivity : AppCompatActivity() {
         background = Drawing(this)
         mainLayout.addView(background)
 
+        setPaint(Color.BLACK)
+
+        //region MotionEvents
         background.setOnTouchListener { _, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
+                    this.background.Paths.add(Path())
+                    this.background.Position += 1
+
                     this.background.StartX = motionEvent.x
                     this.background.StartY = motionEvent.y
 
@@ -43,6 +50,43 @@ class MainActivity : AppCompatActivity() {
             background.invalidate()
             true
         }
+        //endregion
+    }
+
+    //region Button listeners
+    fun drawWhite(v: View) {
+        setPaint(Color.WHITE)
+
+        this.background.setBackgroundColor(Color.BLACK)
+    }
+
+    fun drawBlack(v: View) {
+        setPaint(Color.BLACK)
+
+        this.background.setBackgroundColor(Color.WHITE)
+    }
+
+    fun drawBlue(v: View) {
+        setPaint(Color.BLUE)
+    }
+
+    fun drawGreen(v: View) {
+        setPaint(Color.GREEN)
+    }
+
+    fun drawRed(v: View) {
+        setPaint(Color.RED)
+
+    }
+    //endregion
+
+    private fun setPaint(paintColor: Int) {
+        background.Paints.add(Paint().apply {
+            color = paintColor
+            style = Paint.Style.STROKE
+            strokeWidth = 12f
+            isAntiAlias = true
+        })
     }
 
     class Drawing(context: Context) : View(context) {
@@ -52,14 +96,16 @@ class MainActivity : AppCompatActivity() {
         private var endX: Float = 0f
         private var endY: Float = 0f
 
-        private val paint = Paint().apply {
-            color = Color.BLACK
-            style = Paint.Style.STROKE
-            strokeWidth = 12f
-            isAntiAlias = true
-        }
+        private var position: Int = -1
 
-        private var path: Path = Path()
+        private var bitmap: Bitmap? = null
+
+        //region Particular drawing
+        private var paints: MutableList<Paint> = mutableListOf<Paint>()
+        private val paths: MutableList<Path> = mutableListOf<Path>()
+//        private val pathPaint: MutableMap<Int, Int> = mutableMapOf<Int, Int>()
+
+        //endregion
 
         //region Properties
         var StartX: Float
@@ -86,9 +132,22 @@ class MainActivity : AppCompatActivity() {
                 this.endY = value
             }
 
-        val Path: Path
-            get() = this.path
+        var Position: Int
+            get() = this.position
+            set(value) {
+                this.position = value
+            }
+
+        val Paths: MutableList<Path>
+            get() = this.paths
+
+        val Paints: MutableList<Paint>
+            get() = this.paints
         //endregion
+
+        init {
+            bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
+        }
 
         override fun performClick(): Boolean {
             return super.performClick()
@@ -100,34 +159,45 @@ class MainActivity : AppCompatActivity() {
          * Method that sets the initial/start position of drawing.
          */
         fun moveDown(x: Float, y: Float) {
-            this.Path.moveTo(x, y)
+            this.Paths[position].moveTo(x, y)
         }
 
         /**
          * Method that specifies the "road" of a drawing.
          */
         fun moveForward(startX: Float, startY: Float) {
-            this.Path.quadTo(startX, startY, startX, startY)
+            this.Paths[position].quadTo(startX, startY, startX, startY)
         }
+
 
         /**
          * Method that sets the end position of drawing.
          */
         fun moveUp(x: Float, y: Float) {
-            this.Path.lineTo(x, y)
+            this.Paths[position].lineTo(x, y)
         }
+
 
         //endregion
 
         override fun onDraw(canvas: Canvas) {
             super.onDraw(canvas)
+            canvas.drawBitmap(this.bitmap!!, 100f, 0f, null)
 
-            canvas.drawPath(this.Path, this.paint)
+            println("POSITION :: $position")
+            println("PATH COUNT :: ${Paths.count()}")
+            println("PAINT COUNT :: ${Paths.count()} \n\n")
 
-            // Draw circles at the start and end of touch
-            // TODO("Create a new Paint object with dot setting and new path for dot so the dot won't be disappearing")
-            canvas.drawCircle(StartX, StartY, 20f, paint)
-            canvas.drawCircle(EndX, EndY, 20f, paint)
+            try {
+                // Creating particular path with it particular paint set.
+                for(i in 0..this.Paths.count() step 1) {
+                    canvas.drawPath(paths[i], paints[i])
+                }
+            } catch (e: IndexOutOfBoundsException) {
+                println("Error occurred: ${e.message}")
+            } finally {
+                canvas.save()
+            }
         }
     }
 }
